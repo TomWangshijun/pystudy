badc36e6f92852b220762b0f9d9afe46f67f0279
wsj <wsj@wsjdeMacBook-Air.local>
2018-01-23 23:43:56 +0800

    first time

---
+++ pysqlite3.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+#connect to sqlite3 
+#database file is test.db
+#if not exit, it will be created
+
+testconn= sqlite3.connect('test.db')
+#create a cursor
+#cursor = testconn.cursor()
+# excute a sql 
+#cursor.execute('create table user(id varchar(20) primary key, name varchar(20))')
+#cursor.execute('insert into user(id,name) values (\'13\',\'Tom Wang\')')
+#print cursor.rowcount
+#testconn.commit()
+#cursor.close()
+cursor=testconn.cursor()
+cursor.execute('select * from user')
+values = cursor.fetchall()
+cursor.close()
+testconn.close()
+print values
+
